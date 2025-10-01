073d5cc43d33640d99a6156d94f005391b73bc65
Tom Deneire <tomdeneire@gmail.com>
2025-10-01 16:26:57 +0200

    (style): nicer style params

--- test.lua
+++ test.lua
@@ -2,8 +2,15 @@ local fontasy = require("lua.fontasy")
 
 -- test
 local input = "abcdefghijklmnopqrstuvwxyz 0123456789 +-=?!#%<>,.:"
-local flavours = { "circles", "circles_outline", "box", "box_outline", "numeric", "numeric_box_multiple",
-    "numeric_box_multiple_outline" }
+local flavours = {
+    "circles",
+    "circles_outline",
+    "box",
+    "box_outline",
+    "numeric",
+    "numeric_box_multiple",
+    "numeric_box_multiple_outline"
+}
 local spacings = { 0, 1 }
 
 for _, flavour in ipairs(flavours) do
