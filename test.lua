472dcb632f81bbc637a07a4d5afab24ffbbd3515
Tom Deneire <tomdeneire@gmail.com>
2025-10-01 16:25:14 +0200

    (feat): extra test chars

--- test.lua
+++ test.lua
@@ -1,7 +1,7 @@
 local fontasy = require("lua.fontasy")
 
 -- test
-local input = "abcdefghijklmnopqrstuvwxyz 0123456789 +-=#,.:"
+local input = "abcdefghijklmnopqrstuvwxyz 0123456789 +-=?!#%<>,.:"
 local flavours = { "circles", "circles_outline", "box", "box_outline", "numeric", "numeric_box_multiple",
     "numeric_box_multiple_outline" }
 local spacings = { 0, 1 }
